c3b63c5c4e93a08e05b81224ccdb105e069df5b3
UdayaMarvel <udayamarvel@gmail.com>
2024-09-25 11:31:32 +0530

    Revert "new line"
    This reverts commit 7c153f2edb0c08ed5a4432e24bfba8a7ba883edb.

--- src/test/java/com/common/amazon/CommonActions.java
+++ src/test/java/com/common/amazon/CommonActions.java
@@ -6,7 +6,6 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -63,6 +62,6 @@ public class CommonActions {
 	}
 
 	public void methodForChecking() {
-		WebElement ele = driver.findElement(By.xpath(""));
+		
 	}
 }
