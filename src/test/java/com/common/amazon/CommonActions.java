a5b63db81f82f9e2bd5b2ba121148d8b8c160961
Ela93 <elavarasan665@gmail.com>
2024-09-26 02:51:44 +0530

    java class

--- src/test/java/com/common/amazon/CommonActions.java
+++ src/test/java/com/common/amazon/CommonActions.java
@@ -1,21 +1,7 @@
 package com.common.amazon;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
-
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.interactions.Actions;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class CommonActions {
+ 
 	public static class Palindrome {
 
 		public static void main(String[] args) {
